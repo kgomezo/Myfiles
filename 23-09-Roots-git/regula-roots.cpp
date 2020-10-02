c7caacefe5449f4ea50383c8b0ad2c0fedb09d0e
kgomezo <live@slackware.example.org>
2020-10-02 08:25:03 -0500

    regula fin

--- 23-09-Roots-git/regula-roots.cpp
+++ 23-09-Roots-git/regula-roots.cpp
@@ -4,6 +4,7 @@
 using fptr = double(double);
 double f(double x);
 double bisection(double xl, double xu, double eps, fptr fun);
+double regulafalsi(double xl, double xu, double eps, fptr fun);
 
 int main(int argc, char **argv)
 {
@@ -14,6 +15,8 @@ int main(int argc, char **argv)
 
   double xroot = bisection(xl, xu, eps, f);
   std::cout << xroot <<"\t" << f(xroot) <<"\n";
+  xroot = regulafalsi (xl, xu, eps, f);
+  std::cout << xroot << "\t" << f(xroot) <<"\n";
 
 
   return 0;
@@ -37,6 +40,7 @@ double bisection(double xl, double xu, double eps, fptr fun){
   
   while (niter <= NITERMAX){
     xr = 0.5*(xl+xu); //candidato a la raiz
+
     if(std::fabs(f(xr)) <= eps){ //si la f es muy chiquita ya termino
        break;
     }
@@ -48,7 +52,30 @@ double bisection(double xl, double xu, double eps, fptr fun){
     }
     niter++;
   }
-  std::cout <<"Bisection Info -> Niter: " << niter <<"\n";
+  std::cout <<"Bisection Info -> Niter: " << niter<<" " << xl <<" " << xu << " " << xr << " " << f(xr) <<"\n";
   return xr;
 
 }
+
+double regulafalsi(double xl, double xu, double eps, fptr fun){
+  const int NITERMAX=1000;
+  double xr = 0;
+  int niter = 0;
+  while (niter <= NITERMAX){
+    double fxu= fun(xu);
+    double fxl= fun(xl);
+    xr= xu - (fxu*(xl - xu))/(fxl-fxu);
+    if(std::fabs(fun(xr)) <= eps){ break; }
+    else if (fun(xr)*fun(xu)> 0){ xu= xr; }
+    else { xl = xr; }
+    niter++;
+  }
+
+  std::cout <<"Regula falsi Info -> Niter: "<< niter <<"\n";
+  return xr;
+}
+
+  
+
+
+
